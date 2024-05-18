21b5755920411baaa1d713167af412fd3b25be04
htetnan <htetnandaraung2002@gmail.com>
2024-05-18 22:35:53 +0630

    add map to the listing page using leaflet and react-leaflet

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -79,7 +79,7 @@ return <Spinner/>;
       )}
 
       <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
-        <div className=" w-full h-[200px] lg-[400px] ">
+        <div className=" w-full">
           <p className="text-2xl font-bold mb-3 text-blue-900"> 
             {listing.name } - MMK {listing.offer ? listing.discountedPrice.toString()
                   .replace(/\B(?=(\d{3})+(?!\d))/g, ",") 
@@ -98,7 +98,7 @@ return <Spinner/>;
 )}
           </div>
           <p className="mt-3 mb-3">Description -<span className="font-semibold">{listing.description}</span></p>
-          <ul className="flex items-center space-x-2 sn:space-x-10 text-sm font-semibold">
+          <ul className="flex items-center space-x-2 sm:space-x-10 text-sm font-semibold mb-6">
             <li className="flex items-center whitespace-nowrap">
               <FaBed className="text-lg mr-1"/>
               {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
@@ -135,12 +135,25 @@ return <Spinner/>;
             <Contact userRef={listing.userRef} listing={listing} />
           )}
         </div>
-        <div className=" bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden">
- 
-       
-
-
-
+        <div className="w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2">
+          <MapContainer
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>
+                {listing.address}
+              </Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
   </main>;
